6241ed4bde6932c383847120579e9204709c6506
Tom North <tnorth93@gmail.com>
2020-06-23 14:21:45 -0700

    dynamically adding blog posts from data retrieved

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,20 +7,27 @@ import './Blog.css';
 import Axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+    }
+
     componentDidMount () {
         Axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                this.setState({posts: response.data })
+                // console.log(response);
             });
     }
 
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title}/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
