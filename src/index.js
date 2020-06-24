f195d12dc0199ae62cf07a0b30a16af36b8c9767
Tom North <tnorth93@gmail.com>
2020-06-23 17:22:47 -0700

    added instances for fun

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 let requestInterceptor = axios.interceptors.request.use(request => {
