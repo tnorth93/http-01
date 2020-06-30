98a6a4e77c786e9231f19266fb31e430d106797d
Tom North <tnorth93@gmail.com>
2020-06-30 16:06:22 -0700

    added home and new post links

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -11,7 +11,7 @@ class FullPost extends Component {
     componentDidUpdate () {
         if (this.props.id) {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+                axios.get('/posts/git' + this.props.id)
                     .then(response => {
                         this.setState({loadedPost: response.data});
                     })
