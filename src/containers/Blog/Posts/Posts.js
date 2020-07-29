1c0f37d60a71b3fd20697299fa40e260257c5a65
Tom North <tnorth93@gmail.com>
2020-07-29 16:38:45 -0700

    canged posts.js to return a post wrapped with a dynamic id based link

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
 import axios from '../../../axios';
+import { Link } from 'react-router-dom';
 import './Posts.module.css';
 
 class Posts extends Component {
@@ -36,11 +37,15 @@ class Posts extends Component {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
-        return <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}/>
+        return (
+          <Link to={'/' + post.id}>
+            <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}/>
+          </Link>
+        )
       });
 
     return (
